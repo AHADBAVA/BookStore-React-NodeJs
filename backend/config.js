4d254e365b265e7ee2cbcd8b73e67f82f71abdc3
Abdul Ahad S <139561652+AHADBAVA@users.noreply.github.com>
2024-04-29 16:52:29 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://ahaddesgins:gKBRL1QsUTuqWUvI@cluster0.pnd9mpy.mongodb.net/Debugmedia?retryWrites=true&w=majority&appName=Cluster0";
+export const mongoDBURL = "Add Your Mongo Atlas URL";
